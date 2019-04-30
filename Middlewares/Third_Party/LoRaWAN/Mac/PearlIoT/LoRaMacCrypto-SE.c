03183c18b3787de093f9b888d037ac765183a621
abdalabd <abdallah.abdelhamid@idemia.com>
2019-04-30 12:10:04 +0200

    Add CMakefile.txt

---
+++ Middlewares/Third_Party/LoRaWAN/Mac/PearlIoT/LoRaMacCrypto-SE.c
@@ -0,0 +1,229 @@
+/*!
+ * \file      LoRaMacCrypto-SE.c
+ *
+ * \brief     LoRa MAC layer cryptography implementation
+ *
+ * \copyright Revised BSD License, see section \ref LICENSE.
+ *
+ * \code
+ *                ______                              _
+ *               / _____)             _              | |
+ *              ( (____  _____ ____ _| |_ _____  ____| |__
+ *               \____ \| ___ |    (_   _) ___ |/ ___)  _ \
+ *               _____) ) ____| | | || |_| ____( (___| | | |
+ *              (______/|_____)_|_|_| \__)_____)\____)_| |_|
+ *              (C)2013-2017 Semtech
+ *
+ *               ___ _____ _   ___ _  _____ ___  ___  ___ ___
+ *              / __|_   _/_\ / __| |/ / __/ _ \| _ \/ __| __|
+ *              \__ \ | |/ _ \ (__| ' <| _| (_) |   / (__| _|
+ *              |___/ |_/_/ \_\___|_|\_\_| \___/|_|_\\___|___|
+ *              embedded.connectivity.solutions===============
+ *
+ * \endcode
+ *
+ * \author    Miguel Luis ( Semtech )
+ *
+ * \author    Gregory Cristian ( Semtech )
+ *
+ * \author    Daniel Jaeckle ( STACKFORCE )
+ *
+ * \author    Johannes Bruder ( STACKFORCE )
+ *
+ * \author    Francois Lorrain ( IDEMIA )
+ *
+ *  */
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "utilities.h"
+// #include "secure-element.h"
+
+#include "LoRaMacParser.h"
+#include "LoRaMacSerializer.h"
+#include "LoRaMacCrypto.h"
+
+#include "../../../peripherals/SE_API/LORA/SE_API_ITF_LoRa.h"
+
+/*
+ * Initial value of the frame counters
+ */
+#define FCNT_DOWN_INITAL_VALUE          0xFFFFFFFF
+
+/*
+ * Frame direction definition for uplink communications
+ */
+#define UPLINK                          0
+
+/*
+ * Frame direction definition for downlink communications
+ */
+#define DOWNLINK                        1
+
+/*
+ * CMAC/AES Message Integrity Code (MIC) Block B0 size
+ */
+#define MIC_BLOCK_BX_SIZE               16
+
+/*
+ * Size of JoinReqType is field for integrity check
+ */
+#define JOIN_REQ_TYPE_SIZE              1
+
+/*
+ * Size of DevNonce is field for integrity check
+ */
+#define DEV_NONCE_SIZE                  2
+
+/*
+ * Number of security context entries
+ */
+#define NUM_OF_SEC_CTX                  5
+
+/*
+ * Maximum size of the message that can be handled by the crypto operations
+ */
+#define CRYPTO_MAXMESSAGE_SIZE          256
+
+/*
+ * Maximum size of the buffer for crypto operations
+ */
+#define CRYPTO_BUFFER_SIZE              CRYPTO_MAXMESSAGE_SIZE + MIC_BLOCK_BX_SIZE
+
+/*
+ * MIC computaion offset
+ */
+#define CRYPTO_MIC_COMPUTATION_OFFSET   JOIN_REQ_TYPE_SIZE + LORAMAC_JOIN_EUI_FIELD_SIZE + DEV_NONCE_SIZE + LORAMAC_MHDR_FIELD_SIZE
+
+
+/*
+ *  API functions
+ */
+
+LoRaMacCryptoStatus_t LoRaMacCryptoInit( EventNvmCtxChanged cryptoNvmCtxChanged )
+{
+// Need to power up the Pearl IoT
+
+    return LORAMAC_CRYPTO_SUCCESS;
+}
+
+LoRaMacCryptoStatus_t LoRaMacCryptoSetLrWanVersion( Version_t version )
+{
+    return LORAMAC_CRYPTO_SUCCESS;
+}
+
+LoRaMacCryptoStatus_t LoRaMacCryptoRestoreNvmCtx( void* cryptoNvmCtx )
+{
+   return LORAMAC_CRYPTO_SUCCESS;
+}
+
+void* LoRaMacCryptoGetNvmCtx( size_t* cryptoNvmCtxSize )
+{
+    return LORAMAC_CRYPTO_SUCCESS;
+}
+
+LoRaMacCryptoStatus_t LoRaMacCryptoSetKey( KeyIdentifier_t keyID, uint8_t* key )
+{
+    return LORAMAC_CRYPTO_SUCCESS;
+}
+
+LoRaMacCryptoStatus_t LoRaMacCryptoPrepareJoinRequest( LoRaMacMessageJoinRequest_t* macMsg )
+{
+	uint16_t length=256;
+	uint16_t status;
+    if( macMsg == 0 )
+    {
+        return LORAMAC_CRYPTO_ERROR_NPE;
+    }
+    status = SE_API_ITF_LoRa_join_request(macMsg->MHDR.Value, macMsg->Buffer, &length);
+    macMsg->BufSize = length;
+    if (status == SE_API_SUCCESS) {
+    	return LORAMAC_CRYPTO_SUCCESS;
+    } else {
+    	return status;
+    }
+}
+
+LoRaMacCryptoStatus_t LoRaMacCryptoPrepareReJoinType1( LoRaMacMessageReJoinType1_t* macMsg )
+{
+    if( macMsg == 0 )
+    {
+        return LORAMAC_CRYPTO_ERROR_NPE;
+    }
+
+    return LORAMAC_CRYPTO_SUCCESS;
+}
+
+LoRaMacCryptoStatus_t LoRaMacCryptoPrepareReJoinType0or2( LoRaMacMessageReJoinType0or2_t* macMsg )
+{
+    if( macMsg == 0 )
+    {
+        return LORAMAC_CRYPTO_ERROR_NPE;
+    }
+    return LORAMAC_CRYPTO_SUCCESS;
+}
+
+LoRaMacCryptoStatus_t LoRaMacCryptoHandleJoinAccept( JoinReqIdentifier_t joinReqType, uint8_t* joinEUI, LoRaMacMessageJoinAccept_t* macMsg )
+{
+	uint16_t length = 256, status;
+
+	if( ( macMsg == 0 ) )
+    {
+        return LORAMAC_CRYPTO_ERROR_NPE;
+    }
+    status = SE_API_ITF_LoRa_join_accept(macMsg->Buffer, macMsg->BufSize, macMsg->Buffer, &length);
+    if (status != SE_API_SUCCESS) {
+    	return status;
+    }
+
+    // Parse the message
+    if( LoRaMacParserJoinAccept( macMsg ) != LORAMAC_PARSER_SUCCESS )
+    {
+        return LORAMAC_CRYPTO_ERROR_PARSER;
+    }
+
+
+    return LORAMAC_CRYPTO_SUCCESS;
+}
+
+LoRaMacCryptoStatus_t LoRaMacCryptoSecureMessage( uint32_t fCntUp, uint8_t txDr, uint8_t txCh, LoRaMacMessageData_t* macMsg )
+{
+    return LORAMAC_CRYPTO_SUCCESS;
+}
+
+LoRaMacCryptoStatus_t LoRaMacCryptoUnsecureMessage( AddressIdentifier_t addrID, uint32_t address, FCntIdentifier_t fCntID, uint32_t fCntDown, LoRaMacMessageData_t* macMsg )
+{
+    if( macMsg == 0 )
+    {
+        return LORAMAC_CRYPTO_ERROR_NPE;
+    }
+
+    return LORAMAC_CRYPTO_SUCCESS;
+}
+
+LoRaMacCryptoStatus_t LoRaMacCryptoDeriveMcKEKey( KeyIdentifier_t keyID, uint16_t nonce, uint8_t* devEUI )
+{
+    if( devEUI == 0 )
+    {
+        return LORAMAC_CRYPTO_ERROR_NPE;
+    }
+
+    // Nonce SHALL be greater than 15
+    if( nonce < 16 )
+    {
+        return LORAMAC_CRYPTO_FAIL_PARAM;
+    }
+
+    return LORAMAC_CRYPTO_SUCCESS;
+}
+
+LoRaMacCryptoStatus_t LoRaMacCryptoDeriveMcSessionKeyPair( AddressIdentifier_t addrID, uint32_t mcAddr )
+{
+    if( mcAddr == 0 )
+    {
+        return LORAMAC_CRYPTO_ERROR_NPE;
+    }
+
+     return LORAMAC_CRYPTO_SUCCESS;
+}
